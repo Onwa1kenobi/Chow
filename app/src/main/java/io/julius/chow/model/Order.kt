97b5441689e2ef8c5c9e7a9497858ff099e4918d
ugochukwu <amehugochukwu@gmail.com>
2019-05-27 02:18:10 +0100

    Fix order item layout distortion

--- app/src/main/java/io/julius/chow/model/Order.kt
+++ app/src/main/java/io/julius/chow/model/Order.kt
@@ -1,8 +1,6 @@
 package io.julius.chow.model
 
 import androidx.lifecycle.MutableLiveData
-import java.math.RoundingMode
-import java.text.DecimalFormat
 
 class Order(
     val food: Food,
@@ -10,9 +8,10 @@ class Order(
 ) {
     // Data binding live data variables for UI display
     val liveQuantity = MutableLiveData<Int>().apply { postValue(quantity) }
-    val liveCost = MutableLiveData<String>().apply {
-        postValue(DecimalFormat("#.##").apply {
-            roundingMode = RoundingMode.CEILING
-        }.format(quantity * food.price))
+    val liveCost = MutableLiveData<Double>().apply {
+        postValue((quantity * food.price))
+//        postValue(DecimalFormat("0.00").apply {
+//            roundingMode = RoundingMode.CEILING
+//        }.format(quantity * food.price))
     }
 }

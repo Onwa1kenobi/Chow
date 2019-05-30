9eadbbcf11374961307eed8b067e1eb12affff60
ugochukwu <amehugochukwu@gmail.com>
2019-05-30 01:28:13 +0100

    Update CirceCI config file

--- data/src/androidTest/java/io/julius/chow/data/ExampleInstrumentedTest.java
+++ data/src/androidTest/java/io/julius/chow/data/ExampleInstrumentedTest.java
@@ -1,18 +1,19 @@
 package io.julius.chow.data;
 
 import android.content.Context;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {

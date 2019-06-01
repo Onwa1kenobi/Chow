d733c52830c587b9dd6ae151256162dc61b3003c
ugochukwu <amehugochukwu@gmail.com>
2019-06-01 23:27:18 +0100

    Add order confirmation fragment

---
+++ app/src/main/java/io/julius/chow/main/orders/ConfirmOrderFragment.kt
@@ -0,0 +1,23 @@
+package io.julius.chow.main.orders
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import io.julius.chow.R
+
+
+class ConfirmOrderFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_confirm_order, container, false)
+    }
+
+
+}

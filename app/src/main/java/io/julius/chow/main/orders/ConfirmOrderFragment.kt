b887d650f239c98459820c5d010ce78da2acb994
ugochukwu <amehugochukwu@gmail.com>
2019-06-04 06:32:33 +0100

    Add change address to order confirmation

--- app/src/main/java/io/julius/chow/main/orders/ConfirmOrderFragment.kt
+++ app/src/main/java/io/julius/chow/main/orders/ConfirmOrderFragment.kt
@@ -5,11 +5,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import io.julius.chow.R
 import io.julius.chow.util.RoundedBottomSheetDialogFragment
+import kotlinx.android.synthetic.main.fragment_confirm_order.*
 
 
 class ConfirmOrderFragment : RoundedBottomSheetDialogFragment() {
+
+    private val editing = MutableLiveData<Boolean>().apply {
+        value = false
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +24,29 @@ class ConfirmOrderFragment : RoundedBottomSheetDialogFragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_confirm_order, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button_change_address.setOnClickListener {
+            editing.value = editing.value!!.not()
+        }
+
+        editing.observe(this, Observer {
+            button_place_order.isEnabled = it.not()
+            when (it) {
+                true -> {
+                    button_change_address.text = getString(R.string.done)
+                    field_user_address.isEnabled = true
+                }
+
+                false -> {
+                    button_change_address.text = getString(R.string.change)
+                    field_user_address.isEnabled = false
+                }
+            }
+        })
     }
 
 }

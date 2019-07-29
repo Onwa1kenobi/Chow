62c5f4d2401d13aa11d58b97e8b6bfdd83ed1361
ugochukwu <amehugochukwu@gmail.com>
2019-07-29 23:55:01 +0100

    Add restaurant repository

---
+++ data/src/main/java/io/julius/chow/data/repository/RestaurantRepositoryImpl.kt
@@ -0,0 +1,125 @@
+package io.julius.chow.data.repository
+
+import android.annotation.SuppressLint
+import io.julius.chow.data.mapper.FoodEntityMapper
+import io.julius.chow.data.mapper.RestaurantEntityMapper
+import io.julius.chow.data.source.DataSource
+import io.julius.chow.data.source.DataSourceQualifier
+import io.julius.chow.data.source.Source
+import io.julius.chow.domain.Exception
+import io.julius.chow.domain.Result
+import io.julius.chow.domain.model.FoodModel
+import io.julius.chow.domain.model.RestaurantModel
+import io.julius.chow.domain.repository.ChowRepository
+import io.julius.chow.domain.repository.RestaurantRepository
+import io.reactivex.Flowable
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * Provides an implementation of the [ChowRepository] interface for communicating to and from
+ * Data sources
+ */
+@Singleton
+@SuppressLint("CheckResult")
+class RestaurantRepositoryImpl @Inject constructor(
+    @DataSourceQualifier(Source.Local)
+    private val localDataSource: DataSource,
+    @DataSourceQualifier(Source.Remote)
+    private val remoteDataSource: DataSource
+) : RestaurantRepository {
+
+    override suspend fun authenticateUser(): Result<RestaurantModel> {
+        return remoteDataSource.authenticateRestaurant()
+    }
+
+    override suspend fun getCurrentRestaurant(): Flowable<Result<RestaurantModel>> {
+        return localDataSource.getCurrentRestaurant().map {
+            when (it) {
+                is Result.Success -> {
+                    Result.Success(RestaurantEntityMapper.mapFromEntity(it.data))
+                }
+                is Result.Failure -> {
+                    Result.Failure(Exception.LocalDataNotFoundException)
+                }
+            }
+        }
+    }
+
+    override suspend fun fetchCurrentRestaurant(): RestaurantModel {
+        val restaurant = localDataSource.fetchCurrentRestaurant()
+        return RestaurantEntityMapper.mapFromEntity(restaurant)
+    }
+
+    override suspend fun saveRestaurant(restaurantModel: RestaurantModel): Result<Boolean> {
+        remoteDataSource.saveRestaurant(RestaurantEntityMapper.mapToEntity(restaurantModel))
+        return saveRestaurantLocally(restaurantModel)
+    }
+
+    override suspend fun saveRestaurantLocally(restaurantModel: RestaurantModel): Result<Boolean> {
+        return localDataSource.saveRestaurant(RestaurantEntityMapper.mapToEntity(restaurantModel))
+    }
+
+    override suspend fun fetchRestaurants(): Flowable<Result<List<RestaurantModel>>> {
+        // Fetch remote data and save to local storage
+        remoteDataSource.fetchRestaurants().subscribe {
+            when (it) {
+                is Result.Success -> {
+                    localDataSource.saveRestaurants(it.data)
+                }
+
+                is Result.Failure -> {
+                    Result.Failure(Exception.LocalDataNotFoundException)
+                }
+            }
+        }
+
+        // Return local database result
+        return localDataSource.fetchRestaurants()
+            .map {
+                when (it) {
+                    is Result.Success -> {
+                        // Map each restaurant entity to a restaurant model
+                        val restaurants = it.data.map { restaurantEntity ->
+                            RestaurantEntityMapper.mapFromEntity(restaurantEntity)
+                        }
+
+                        Result.Success(restaurants)
+                    }
+                    is Result.Failure -> {
+                        Result.Failure(Exception.LocalDataNotFoundException)
+                    }
+                }
+            }
+    }
+
+    override suspend fun fetchRestaurantMenu(restaurantId: String): Flowable<Result<List<FoodModel>>> {
+        // Fetch remote data and save to local storage
+        remoteDataSource.fetchRestaurantMenu(restaurantId).subscribe {
+            when (it) {
+                is Result.Success -> {
+                    localDataSource.saveFood(it.data)
+                }
+
+                is Result.Failure -> {
+                    Result.Failure(Exception.LocalDataNotFoundException)
+                }
+            }
+        }
+
+        // Return local database result
+        return localDataSource.fetchRestaurantMenu(restaurantId).map {
+            when (it) {
+                is Result.Success -> {
+                    // Map each data entity to a domain model
+                    val menu = it.data.map { foodEntity -> FoodEntityMapper.mapFromEntity(foodEntity) }
+                    Result.Success(menu)
+                }
+
+                is Result.Failure -> {
+                    Result.Failure(Exception.LocalDataNotFoundException)
+                }
+            }
+        }
+    }
+}
---
+++ domain/src/main/java/io/julius/chow/domain/repository/RestaurantRepository.kt
@@ -0,0 +1,28 @@
+package io.julius.chow.domain.repository
+
+import io.julius.chow.domain.Result
+import io.julius.chow.domain.model.FoodModel
+import io.julius.chow.domain.model.RestaurantModel
+import io.reactivex.Flowable
+
+/**
+ * Interface defining methods for how the Domain layer communicates with the Data layer.
+ * This is to be implemented by the Data layer, setting the requirements for the
+ * operations that need to be implemented.
+ */
+interface RestaurantRepository {
+
+    suspend fun authenticateUser(): Result<RestaurantModel>
+
+    suspend fun getCurrentRestaurant(): Flowable<Result<RestaurantModel>>
+
+    suspend fun fetchCurrentRestaurant(): RestaurantModel
+
+    suspend fun saveRestaurant(restaurantModel: RestaurantModel): Result<Boolean>
+
+    suspend fun saveRestaurantLocally(restaurantModel: RestaurantModel): Result<Boolean>
+
+    suspend fun fetchRestaurants(): Flowable<Result<List<RestaurantModel>>>
+
+    suspend fun fetchRestaurantMenu(restaurantId: String): Flowable<Result<List<FoodModel>>>
+}

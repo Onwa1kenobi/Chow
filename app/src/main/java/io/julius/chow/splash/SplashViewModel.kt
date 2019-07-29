7df2f4399662f0a2cc4b0a9d6477516c1f2a0787
ugochukwu <amehugochukwu@gmail.com>
2019-07-29 23:51:37 +0100

    Add account type checker

--- app/src/main/java/io/julius/chow/splash/SplashViewModel.kt
+++ app/src/main/java/io/julius/chow/splash/SplashViewModel.kt
@@ -3,7 +3,6 @@ package io.julius.chow.splash
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.julius.chow.domain.Result
-import io.julius.chow.domain.interactor.Interactor
 import io.julius.chow.domain.interactor.splash.UserPresentInteractor
 import javax.inject.Inject
 
@@ -15,10 +14,14 @@ class SplashViewModel @Inject constructor(private val userPresentInteractor: Use
         isUserLoggedIn()
     }
 
-    private fun isUserLoggedIn() = userPresentInteractor.execute(Interactor.None()) {
+    private fun isUserLoggedIn() = userPresentInteractor.execute(UserPresentInteractor.Params()) {
         when (it) {
             is Result.Success<*> -> userIsLoggedIn.postValue(it.data as Boolean)
             else -> userIsLoggedIn.postValue(false)
         }
+    }
+
+    fun getCurrentLoggedUser(): Any {
+        return userPresentInteractor.execute(UserPresentInteractor.Params(UserPresentInteractor.Params.ParamType.CHECK_USER_TYPE))
     }
 }
